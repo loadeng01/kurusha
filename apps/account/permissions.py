ee3efb18b42f1f2555d6ef31167a65af7cd831d8
Myrza <loadenggg@gmail.com>
2023-12-05 13:30:40 +0600

    adsadads

--- apps/account/permissions.py
+++ apps/account/permissions.py
@@ -11,14 +11,11 @@ class IsAuthorOrAdminOrEmployee(BasePermission):
 
 
 class IsAdminOrEmployee(BasePermission):
-    # def has_object_permission(self, request, view, obj):
-    #     if request.user.is_superuser:
-    #         return True
-    #     if request.user.is_staff:
-    #         return True
-    #     return False
-    def has_permission(self, request, view):
-        return bool(request.user and request.user.is_superuser)
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_superuser
+
+    # def has_permission(self, request, view):
+    #     return bool(request.user and request.user.is_superuser)
 
 
 class IsActive(BasePermission):
